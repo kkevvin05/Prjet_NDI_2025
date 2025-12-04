c9930e54b6bfb2aebb0e947570c064f87dfe2877
Kevin <kevin.mievre@etu.umontpellier.fr>
2025-12-04 21:27:49 +0100

    Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Page1 from './pages/Page1.jsx'
+import Page2 from './pages/Page2.jsx'
+import Page3 from './pages/Page3.jsx'
+import Page4 from './pages/Page4.jsx'
+import Page5 from './pages/Page5.jsx'
+import Page6 from './pages/Page6.jsx' 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Page1 />
+      <Page2 />
+      <Page3 />
+      <Page4 />
+      <Page5 />
+      <Page6 />
     </>
   )
 }
